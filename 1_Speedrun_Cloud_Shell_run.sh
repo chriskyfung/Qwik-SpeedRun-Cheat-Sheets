eebb7a6ff7b588eb5fff4aa715a1f3934af898ba
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2019-10-19 16:00:55 +0800

    beta v2

--- 1_Speedrun_Cloud_Shell_run.sh
+++ 1_Speedrun_Cloud_Shell_run.sh
@@ -1,1 +1,4 @@
-gcloud beta compute --project=qwiklabs-gcp-00-746c8acce1b3 instances create dev-instance --zone=us-central1-f --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=109805325561-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server --image=debian-9-stretch-v20191014 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=dev-instance --reservation-affinity=any
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+export SERVICE_ACCOUNT=$(gcloud config list account --format 'value(core.account)')
+
+gcloud beta compute --project=${PROJECT_ID} instances create dev-instance --zone=us-central1-f --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=${SERVICE_ACCOUNT} --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server --image=debian-9-stretch-v20191014 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=dev-instance --reservation-affinity=any
