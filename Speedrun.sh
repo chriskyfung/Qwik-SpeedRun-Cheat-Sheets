001daadc82dca47d0d74439d21af00044b878a52
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2019-10-19 15:20:58 +0800

    Not very succeed...

---
+++ Speedrun.sh
@@ -0,0 +1,7 @@
+sudo apt-get update &&
+sudo apt-get install git -y && sudo apt-get install -yq openjdk-8-jdk && sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java && sudo apt-get install -yq maven && sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080 && export GCLOUD_PROJECT="$(curl -H Metadata-Flavor:Google http://metadata/computeMetadata/v1/project/project-id)"
+java -version
+git clone -b GSP166 https://github.com/chriskyfung/My-Qwiklabs-SpeedRun-Tank
+cd ~/My-Qwiklabs-SpeedRun-Tank/courses/developingapps/java/devenv/
+mvn clean install
+mvn spring-boot:run
