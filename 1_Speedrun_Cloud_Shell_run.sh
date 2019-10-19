4cb3d91f7de82c5c85a938b35585eb55b5c3779e
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2019-10-19 18:01:19 +0800

    Update 1_Speedrun_Cloud_Shell_run.sh

--- 1_Speedrun_Cloud_Shell_run.sh
+++ 1_Speedrun_Cloud_Shell_run.sh
@@ -1,5 +1,5 @@
-git clone -b GSP168 https://github.com/chriskyfung/My-Qwiklabs-SpeedRun-Tank/
-cd ~/My-Qwiklabs-SpeedRun-Tank/courses/developingapps/java/cloudstorage/start
+git clone -b GSP168 https://github.com/chriskyfung/Qwik-SpeedRun-Cheat-Sheets/
+cd ~/Qwik-SpeedRun-Cheat-Sheets/courses/developingapps/java/cloudstorage/start
 . prepare_environment.sh
 export PROJECT_ID=$(gcloud info --format='value(config.project)')
 gsutil mb gs://${PROJECT_ID}-media
