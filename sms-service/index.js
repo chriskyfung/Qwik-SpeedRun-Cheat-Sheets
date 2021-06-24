9d2372662b0bfd17b73ff322fd937cd7028c599d
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2021-06-24 16:32:15 +0800

    Fix steps

--- sms-service/index.js
+++ sms-service/index.js
@@ -1,28 +1,32 @@
-const {PubSub} = require('@google-cloud/pubsub');
-const pubsub = new PubSub();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-const port = process.env.PORT || 8080;
 
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('Listening on port', port);
 });
 
 app.post('/', async (req, res) => {
+  const labReport = decodeBase64Json(req.body.message.data);
   try {
-    const labReport = req.body;
-    await publishPubSubMessage(labReport);
+    console.log(`SMS Service: Report ${labReport.id} trying...`);
+    sendSms();
+
+    console.log(`SMS Service: Report ${labReport.id} success :-)`);    
     res.status(204).send();
   }
   catch (ex) {
-    console.log(ex);
-    res.status(500).send(ex);
+    console.log(`SMS Service: Report ${labReport.id} failure: ${ex}`);
+    res.status(500).send();
   }
 })
 
-async function publishPubSubMessage(labReport) {
-  const buffer = Buffer.from(JSON.stringify(labReport));
-  await pubsub.topic('new-lab-report').publish(buffer);
+function decodeBase64Json(data) {
+  return JSON.parse(Buffer.from(data, 'base64').toString());
+}
+
+function sendSms() {
+  console.log('Sending SMS');
 }
